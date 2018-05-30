7a2051fc612d604136a8eb12dbdf35aa9bbe13cc
António Lima <limaa91@gmail.com>
2018-05-30 19:19:51 +0100

    added new config file

---
+++ config/goodreads.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'storage' => [
+        'storage_folder' => 'userdata\\',
+        'xml_user_info' => '-user-info',
+        'xml_shelf_read' => '-shelf-read-',
+        'xml_extension' => '.xml',
+    ],
+    'status' => [
+        'profile_valid' => 40,
+        'profile_absent' => 41,
+        'profile_private' => 43,
+        'profile_not_found' => 44,
+    ],
+    'strings' => [
+        'profile_private_message' => 'This profile is currently private.',
+        'profile_not_found_message' => 'Profile not found.',
+    ],
+];
